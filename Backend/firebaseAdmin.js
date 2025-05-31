6e8f03bb4c87bddfd13066e6bfda9fa924012f5c
yogesh-1413 <yogeshwaraododda@gmail.com>
2025-05-31 18:52:48 +0530

    SFR

--- Backend/firebaseAdmin.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-admin-key.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://sara-ai-1413.firebaseio.com'
-});
-
-module.exports = admin;
